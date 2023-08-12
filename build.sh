8a91b3fa0a8a5c2fc7c7afe031d337449e349147
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2023-08-12 22:53:57 +0700

    updated something

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-py -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Migration..."
-py manage.py makemigrations --noinput
-py manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-py manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
